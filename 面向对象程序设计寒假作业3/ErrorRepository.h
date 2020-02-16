f4c1fcbfb606bad9c9c561c82b0dbf02fc035371
JustinRochester <60056121+JustinRochester@users.noreply.github.com>
2020-02-16 11:24:31 +0800

    增加了输出范围

--- 面向对象程序设计寒假作业3/ErrorRepository.h
+++ 面向对象程序设计寒假作业3/ErrorRepository.h
@@ -6,62 +6,45 @@ using namespace std;
 #define ERRORREPOSITORY
 class ErrorRepository{
     private:
-    	vector<string> Keyword;
-        void NotExist(string &output){
-            output="变量不存在";
-        }
-        void Applied(string &output){
-            output="变量已申请";
-        }
-        void NumberError(string &output){
-            output="数字错误";
-        }
-        void DontKnow(string &output){
-            output="语句无法识别";
-        }
-        void ConflictError(string &output){
-            output="与关键字冲突";
-        }
-        void NumberLimitError(string &output){
-            output="数字超限";
-        }
-        void DevidedError(string &output){
-        	output="零不得为除数";
+        vector<string> keywordRepository,errorWordRepository;
+        void KeywordRepositoryPrepare(){
+            keywordRepository.push_back("增加");
+            keywordRepository.push_back("减少");
+            keywordRepository.push_back("乘以");
+            keywordRepository.push_back("除以");
+            keywordRepository.push_back("取模");
+            keywordRepository.push_back("取余");
+            keywordRepository.push_back("看看");
+            keywordRepository.push_back("等于");
+            keywordRepository.push_back("整数");
+            keywordRepository.push_back("删除");
 		}
-        void Unknown(string &output){
-            output="未知错误";
-        }
+		void ErrorWordRepositoryPrepare(){
+			errorWordRepository.push_back("");
+			errorWordRepository.push_back("变量不存在");
+			errorWordRepository.push_back("变量已申请");
+			errorWordRepository.push_back("数字错误");
+			errorWordRepository.push_back("语句无法识别");
+			errorWordRepository.push_back("与关键字冲突");
+			errorWordRepository.push_back("数字超限");
+			errorWordRepository.push_back("零不得为除数");
+		}
     
     public:
         ErrorRepository(){
-        	Keyword.push_back("增加");
-        	Keyword.push_back("减少");
-        	Keyword.push_back("乘以");
-        	Keyword.push_back("除以");
-        	Keyword.push_back("取模");
-        	Keyword.push_back("取余");
-        	Keyword.push_back("看看");
-        	Keyword.push_back("等于");
-        	Keyword.push_back("整数");
-        	Keyword.push_back("删除");
-		}
+        	KeywordRepositoryPrepare();
+        	ErrorWordRepositoryPrepare();
+        }
         ~ErrorRepository() {}
         void ErrorOutput(string &output,int errorType){
-            switch(errorType){
-                case 1:NotExist(output); break;
-                case 2:Applied(output); break;
-                case 3:NumberError(output); break;
-                case 4:DontKnow(output); break;
-                case 5:ConflictError(output); break;
-                case 6:NumberLimitError(output); break;
-                case 7:DevidedError(output); break;
-                default:Unknown(output);
-            }
+        	if(errorType>=0&&errorType<errorWordRepository.size())
+        		output=errorWordRepository[errorType];
+        	else output="未知错误";
         }
         bool IsKeyword(string name){
-        	for(int i=0;i<Keyword.size();i++)
-        		if(Keyword[i]==name) return 1;
-        	return 0;
-		}
+            for(int i=0;i<keywordRepository.size();i++)
+                if(keywordRepository[i]==name) return 1;
+            return 0;
+        }
 };
 #endif
--- 面向对象程序设计寒假作业3/NumberRepository.h
+++ 面向对象程序设计寒假作业3/NumberRepository.h
@@ -1,18 +1,51 @@
 #include<string>
-#include<vector>
+#include<map>
 using namespace std;
 
 #ifndef NUMBERREPOSITORY
 #define NUMBERREPOSITORY
 class NumberRepository{
     private:
-        vector<string> numberChar;
+        map<int,string> numberChar;
+        map<string,int> charNumber;
+        void NumberCharPrepare(){
+        	numberChar[0]="零";
+        	numberChar[1]="一";
+        	numberChar[2]="二";
+        	numberChar[3]="三";
+        	numberChar[4]="四";
+        	numberChar[5]="五";
+        	numberChar[6]="六";
+        	numberChar[7]="七";
+        	numberChar[8]="八";
+        	numberChar[9]="九";
+        	numberChar[10]="十";
+        	numberChar[100]="百";
+        	numberChar[1000]="千";
+        	numberChar[10000]="万";
+        	numberChar[100000000]="亿";
+		}
+		void CharNumberPrepare(){
+			charNumber["零"]=0;
+			charNumber["一"]=1;
+			charNumber["二"]=2;
+			charNumber["三"]=3;
+			charNumber["四"]=4;
+			charNumber["五"]=5;
+			charNumber["六"]=6;
+			charNumber["七"]=7;
+			charNumber["八"]=8;
+			charNumber["九"]=9;
+			charNumber["十"]=10;
+			charNumber["百"]=100;
+			charNumber["千"]=1000;
+			charNumber["万"]=10000;
+			charNumber["亿"]=100000000;
+		}
         bool FindChar(int &number,string num){
-            for(int i=0;i<=10;i++) if( numberChar[i]==num ){
-                number=i;
-                return true;
-            }//遍历寻找
-            return false;//找不到
+        	if( charNumber.find(num)==charNumber.end() ) return 0;//找不到 
+        	number=charNumber[num];
+        	return true;
         }
         bool FormatChar(string &num){
             int temporary[3];
@@ -30,32 +63,52 @@ class NumberRepository{
             }
             return true;
         }
+        void ToCharInThousand(string &cn,int value){
+        	if(value<20){
+        		if(value<=10) cn+=numberChar[value];
+        		else cn+=numberChar[10]+numberChar[value-10];
+        		return ;
+			}
+			bool prefix=0;
+			if(value>=1000) cn+=numberChar[value/1000]+numberChar[1000],prefix=1;
+			else if(prefix&&value%1000!=0) cn+=numberChar[0],prefix=0;
+			value%=1000;
+			if(value>=100) cn+=numberChar[value/100]+numberChar[100],prefix=1;
+			else if(prefix&&value%100!=0) cn+=numberChar[0],prefix=0;
+			value%=100;
+			if(value>=10) cn+=numberChar[value/10]+numberChar[10];
+			else if(prefix&&value%10!=0) cn+=numberChar[0];
+			value%=10;
+			if(value>=1) cn+=numberChar[value];
+		}
 
     public:
         NumberRepository(){//初始化赋值
-            numberChar.push_back("零");
-            numberChar.push_back("一");
-            numberChar.push_back("二");
-            numberChar.push_back("三");
-            numberChar.push_back("四");
-            numberChar.push_back("五");
-            numberChar.push_back("六");
-            numberChar.push_back("七");
-            numberChar.push_back("八");
-            numberChar.push_back("九");
-            numberChar.push_back("十");
+        	NumberCharPrepare();
+        	CharNumberPrepare();
         }
         ~NumberRepository() {}
 
-        bool ToChar(string &cn,int value){
-            if(value<-99||value>99) return false;
-            cn="";
-            if(value<0) cn="负",value=-value;//先判定是否是负数 
-            if(value<=10) cn+=numberChar[value];
-            else if(value<20) cn+=numberChar[10]+numberChar[value-10];//十几的形式
-            else if(value%10==0) cn+=numberChar[value/10]+numberChar[10];//几十的形式
-            else cn+=numberChar[value/10]+numberChar[10]+numberChar[value%10];
-            return true;
+        bool ToChar(string &cn,long long int value){
+        	if(value<0) cn="负",value=-value;
+        	if(value>>31) return false;
+        	else cn="";
+        	int Ge=value%10000,Wan=value/10000%10000,Yi=value/10000/10000;
+        	bool prefix=0;
+        	if(Yi) ToCharInThousand(cn,Yi),cn+=numberChar[100000000],prefix=1;
+        	if(Wan){
+        		if(Wan<1000&&prefix) cn+=numberChar[0];
+        		if(Wan>=10&&Wan<20&&prefix) cn+=numberChar[1];
+        		ToCharInThousand(cn,Wan),cn+=numberChar[10000];
+        		prefix=1;
+			}
+			else if(prefix) cn+=numberChar[0],prefix=0;
+			if(Ge){
+				if(Ge<1000&&prefix) cn+=numberChar[0];
+				if(Ge>=10&&Ge<20&&prefix) cn+=numberChar[1];
+				ToCharInThousand(cn,Ge);
+			}
+			return true;
         }
 
         bool ToNumber(int &value,string name){
